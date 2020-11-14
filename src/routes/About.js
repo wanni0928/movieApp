47b5220ab2b47469c0e7ebdcaa709deb329cb3c7
wanni0928 <chlgudwnd123@gmail.com>
2020-11-14 16:58:53 +0900

    done

---
+++ src/routes/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+import App from "../App";
+import { HashRouter, Route } from "react-router-dom";
+
+function About() {
+    return <span>About this page</span>
+}
+
+export default App;
